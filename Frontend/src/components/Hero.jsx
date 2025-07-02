2f63864f324ed08c558947fd7729902718b57347
Aniket04080218 <gupta.aniket0408@gmail.com>
2025-07-02 23:48:33 +0530

    update

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = ({title, imageUrl}) => {
     <div className='hero container'>
         <div className="banner">
             <h1>{title}</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, aut pariatur totam incidunt atque, eligendi quod cumque quo ipsa deserunt laudantium illum id provident repellat necessitatibus voluptates enim libero temporibus.</p>
+            <p>your trusted partner in health and healing. We are committed to delivering compassionate, patient-centered care backed by modern medical technology and a team of dedicated professionals. Whether it’s routine check-ups, emergency services, or specialized treatments, we ensure every patient receives personalized attention and the highest standard of care. At Anii Care, your well-being is our priority. We believe in healing with empathy, treating with expertise, and caring like family. Step into a space where medical excellence meets heartfelt compassion — because your health deserves nothing less.</p>
             
         </div>
         <div className="banner">
