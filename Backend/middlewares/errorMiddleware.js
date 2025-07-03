4c4931ee3253845736fc6c5a83ec511770656bcb
Aniket04080218 <gupta.aniket0408@gmail.com>
2025-07-04 00:01:36 +0530

    update middleware

--- Backend/middlewares/errorMiddleware.js
+++ Backend/middlewares/errorMiddleware.js
@@ -11,5 +11,22 @@ export const errorMiddleware = (err, req, res, next) =>{
     err.message = err.message || "Internal Server Error";
     err.statusCode = err.statusCode || 500;
 
+    if(err.code === 11000){
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+        err = new ErrorHandler(message, 400);
+    }
+    if (err.name === "JsonWebTokenErr"){
+        const message = "Your token is invalid. Please log in";
+        err = new ErrorHandler(message, 400);
+    }
+    if (err.name === "TokenExpiredError"){
+        const message = "Your token is expired. ";
+        err = new ErrorHandler(message, 400);
+    }
+    if (err.name === "CastError"){
+        const message = `Invalid ${err.path}`;
+        err = new ErrorHandler(message, 400);
+    }
+
 
 }
